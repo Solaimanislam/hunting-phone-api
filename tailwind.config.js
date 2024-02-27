3ff2ce2c1f180f325e8ec1d666b0faf864faf9f8
Solaimanislam <islamsi2001@gmail.com>
2024-02-28 01:50:58 +0600

    added Phone hunting site

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+// const div = document.getElementById('phone-user');
+//     // console.log(phones);
+//     for(const phone of phones){
+//         console.log(phone);
+//         const pDiv = document.createElement('div');
+//         pDiv.innerHTML = `
+//         <h2>Brand Name: ${phone.brand} </h2>
+//         <h4>Phone Name: ${phone.phone_name} </h4>
+//         `;
+//         div.appendChild(pDiv);
+//     }
